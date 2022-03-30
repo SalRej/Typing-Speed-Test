083be3339b9b8fdad68ab9a148d6cb0d815efc85
Salih <salih1010@abv.bg>
2022-03-30 22:10:30 +0300

    Added fethcin a random words api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 
 function App() {
-  const [currentWord,setCurrentWord] = useState();
-  const handleTyping = ()=>{
-
+  const [currentWord,setCurrentWord] = useState("");
+  const [words,setWords] = useState([]);
+  const handleTyping = (e)=>{
+      setCurrentWord(e.target.value);
   }
+
+  useEffect(()=>{
+    fetch(`https://random-word-api.herokuapp.com/word?number=10`)
+    .then(res=>res.json())
+    .then(res=>{
+      setWords(res);
+    })
+  },[])
   return (
     <div>
-        <input type="text" value={currentWord} onChange={handleTyping}/>
+        <input className="test" type="text" value={currentWord} onChange={handleTyping}/>
+        <input className="test2" disabled={true} value={words.join(" ")}/>
+        <p>{currentWord}</p>
     </div>
   )
 }

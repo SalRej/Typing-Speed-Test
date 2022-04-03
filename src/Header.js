efb93d27ccdd34350625775f9e6bfb8383dd1f03
Salih <salih1010@abv.bg>
2022-04-03 18:02:43 +0300

    added header and styles to it

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="header">
+        <div className='logo'>Logo</div>
+        <div className='header-menu'>
+            <p>Product</p>
+            <p>Pricing</p>
+            <p>Marketplace</p>
+            <p>Community</p>
+            <p>Customers</p>
+            <p>Support</p>
+        </div>
+        <div className='try-it'>Try Live Chat</div>
+    </div>
+  )
+}
+
+export default Header

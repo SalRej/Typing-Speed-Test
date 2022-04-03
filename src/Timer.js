c6df0ef61a622d316a4ca126962a118c4a64c850
SalRej <sal3wwm@gmail.com>
2022-04-03 11:52:31 +0300

    Replaced old timer with on from npm , did the changes to make it work.

--- src/Timer.js
+++ src/Timer.js
@@ -1,8 +1,36 @@
+import { render } from '@testing-library/react';
 import React from 'react'
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 
 function Timer(props) {
+    const renderTime = ({ remainingTime }) => {
+        if (remainingTime === 0) {
+          return <div className="timer">Finish...</div>;
+        }
+      
+        return (
+          <div className="timer">
+            <div className="text">Remaining</div>
+            <div className="value">{remainingTime}</div>
+            <div className="text">seconds</div>
+          </div>
+        );
+    };
+
   return (
     <div>
+        <CountdownCircleTimer
+            key={props.timerRestart}
+            isPlaying={props.started}
+            duration={60}
+            colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+            colorsTime={[7, 5, 2, 0]}
+            onComplete={()=>{
+                props.toogleFinish()
+            }}
+        >
+            {renderTime}
+        </CountdownCircleTimer>
         <div>
             Timer:{props.timeLeft}
         </div>

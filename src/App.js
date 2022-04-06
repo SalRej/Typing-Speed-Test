c9a582b4d7c6758d7f8ae0c59e14abf692e4b4d5
SalRej <sal3wwm@gmail.com>
2022-04-06 11:02:26 +0300

    Replaced unnecessary usStates with useRefs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState,useEffect,useRef} from 'react'
 import Header from './Header';
 import TypingField from './TypingField';
 import Timer from './Timer';
@@ -7,24 +7,26 @@ import Results from './Results';
 function App() {
   const [currentWord,setCurrentWord] = useState("");
   const [words,setWords] = useState([]);
-  const [wordToCheck,setWordToCheck] = useState("");
   const [wordsPassed,setWordsPassed] = useState([]);
-  const [isTypingCorrect,setIsTypingCorrect] = useState(true);
-
+  const [finished,setFinished]= useState(false);
   const [result,setResult] = useState({numCorrectWords:0,numCorrectChars:0,accuracy:0});
-  const [started,setStarted] = useState(false);
-  const [finished,setFinished] = useState(false);
-  const [timerRestart,setTimerRestart] = useState(0);//every time i increment timeRestart the clock restarts
+  
+  const wordToCheck = useRef("");
+  const started = useRef(false);
+  const isTypingCorrect = useRef(true);
+  const timerRestart = useRef(0);//every time i increment timeRestart the clock restarts
 
   const restart =() =>{
     setCurrentWord("");
-    setWordToCheck("");
     setWordsPassed([]);
-    setIsTypingCorrect(true);
-    setResult({numCorrectWords:0,numCorrectChars:0,accuracy:0});
-    setStarted(false);
     setFinished(false);
-    setTimerRestart((prevTime) => prevTime+1);
+    setResult({numCorrectWords:0,numCorrectChars:0,accuracy:0});
+
+    wordToCheck.current="";
+    isTypingCorrect.current = true;
+    started.current=false;
+    timerRestart.current = timerRestart.current + 1;
+
     fetchRandomWords();
   }
   const toogleFinish =()=>{
@@ -32,8 +34,8 @@ function App() {
   }
   const handleTyping = (e)=>{
 
-    if(started===false){
-      setStarted(true);
+    if(started.current===false){
+      started.current=true;
     }
 
     const key = e.key;
@@ -52,10 +54,10 @@ function App() {
     //because the currentWord dosent update imediatly;
     const tempWord = currentWord + key;
     setCurrentWord(currentWord + key);
-    const subString = wordToCheck.substring(0,tempWord.length);
+    const subString = wordToCheck.current.substring(0,tempWord.length);
 
     if(subString === tempWord){
-      setIsTypingCorrect(true);
+      isTypingCorrect.current=true;
       setWords((prevWords)=>{
 
         return prevWords.map((item,index)=>{
@@ -69,34 +71,20 @@ function App() {
         })
       })
     }else{
-      setIsTypingCorrect(false);
+      isTypingCorrect.current=false;
     }
   }
 
   const spacePressed = () =>{
-      //removes the first word from the array 
-      setWords((prevWords)=>{
-        return prevWords.filter((item,index)=>{
-          if(index === 1){
-            setWordToCheck(item);
-          }
-          if(index!==0){
-            return item;
-          }
-        })
-      })
-      //add the word writen in the input in the passed words array
-      //and resets the currentWord
-      setWordsPassed([...wordsPassed,currentWord])
-      setCurrentWord("");
-
+      
       //if the words is corrcet increment the num of correct words and 
       //add to num of correct chars the lenght of the word
       //calulate the accuracy 
-      if(currentWord === wordToCheck){
+      console.log(currentWord + " " +wordToCheck.current)
+      if(currentWord === wordToCheck.current){
 
         const correctWords = result.numCorrectWords+1;
-        const correctChars = result.numCorrectChars+wordToCheck.length;
+        const correctChars = result.numCorrectChars+wordToCheck.current.length;
         console.log(wordsPassed.length)
 
         //im adding 1 to wordsPassed.lenght ,because the wordsPassed is not updated yet
@@ -115,16 +103,31 @@ function App() {
           accuracy:accuracy
         })
       }
+            //add the word writen in the input in the passed words array
+      //and resets the currentWord
+      setWordsPassed([...wordsPassed,currentWord])
+      setCurrentWord("");
+
+      //removes the first word from the array 
+      setWords((prevWords)=>{
+        return prevWords.filter((item,index)=>{
+          if(index === 1){
+            wordToCheck.current=item;
+          }
+          if(index!==0){
+            return item;
+          }
+        })
+      })
   }
   const backspacePressed = () =>{
     if(currentWord.length > 0){
       //note:must return 1 char to the first word of the words array
       //if the currentWord is equl to the wordToCheck
-      const subString = wordToCheck.substring(0,currentWord.length);
+      const subString = wordToCheck.current.substring(0,currentWord.length);
 
       if(subString === currentWord){
-        setIsTypingCorrect(true);
-
+        isTypingCorrect.current=true;
         const lastChar = subString[subString.length-1];
 
         setWords((prevWords)=>{
@@ -137,7 +140,7 @@ function App() {
           })
         })
       }else{
-        setIsTypingCorrect(false);
+       isTypingCorrect.current=false;
       }
       
       //removes the last char from the currentWord-input elemnt
@@ -145,9 +148,9 @@ function App() {
       setCurrentWord(slicedWord);
 
       //chek if after removing 1 char the word will it be correct 
-      const slicedWord2 = wordToCheck.substring(0,slicedWord.length);
+      const slicedWord2 = wordToCheck.current.substring(0,slicedWord.length);
       if(slicedWord2===slicedWord){
-        setIsTypingCorrect(true);
+        isTypingCorrect.current=true;
       }
     }
 
@@ -157,7 +160,7 @@ function App() {
     fetch(`https://random-word-api.herokuapp.com/word?number=200`)
     .then(res=>res.json())
     .then(res=>{
-      setWordToCheck(res[0])
+      wordToCheck.current=res[0];
       setWords(res);
     })
   }
@@ -175,22 +178,22 @@ function App() {
             numCorrectWords={result.numCorrectWords}
             numCorrectChars={result.numCorrectChars}
             accuracy={result.accuracy}
-            started={started}
-            timerRestart={timerRestart}
+            started={started.current}
+            timerRestart={timerRestart.current}
           />
           <TypingField data={{
               currentWord:currentWord,
               words:words,
               wordsPassed:wordsPassed,
-              isTypingCorrect:isTypingCorrect,
+              isTypingCorrect:isTypingCorrect.current,
               finished:finished,
-              started:started
+              started:started.current
             }}
             handler={handleTyping}
           />
 
         </div>
-          { finished===true && <Results 
+          {finished===true && <Results 
             numCorrectWords={result.numCorrectWords}
             numCorrectChars={result.numCorrectChars}
             accuracy={result.accuracy}
